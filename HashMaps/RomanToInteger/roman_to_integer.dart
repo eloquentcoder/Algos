3f43a746aa85bde03d938df34bd8fa4d72813eba
eloquentcoder <eloquentintech@gmail.com>
2022-07-17 15:23:31 +0100

    Update comments for Roman To Integer

--- HashMaps/RomanToInteger/roman_to_integer.dart
+++ HashMaps/RomanToInteger/roman_to_integer.dart
@@ -4,6 +4,7 @@ void main(List<String> args) {
 }
 
 romanToInt(String s) {
+  // since there are no maps in php we use an associative array to map romans to integers
   var romanLiterals = {
     "I": 1,
     "V": 5,
@@ -14,15 +15,21 @@ romanToInt(String s) {
     "M": 1000
   };
 
+// initialize the result
   var total = 0;
 
+  //loop over every character in the string s
   for (var i = 0; i < s.length; i++) {
+    // if the next index is not out of bounds and the character in the roman literal is less than the next character in the roman literal, substract is from total
     if (i + 1 < s.length && romanLiterals[s[i]]! < romanLiterals[s[i + 1]]!) {
       total -= romanLiterals[s[i]]!;
+
+      // if the current character is greater than or equal to the next character add to the total
     } else {
       total += romanLiterals[s[i]]!;
     }
   }
 
+  // return the total
   return total;
 }
--- HashMaps/RomanToInteger/roman_to_integer.js
+++ HashMaps/RomanToInteger/roman_to_integer.js
@@ -1,7 +1,8 @@
 
 
-function romanToInteger(s)
-{
+function romanToInteger(s) {
+    // since there are no maps in php we use an associative array to map romans to integers
+
     let romanLiterals = {
         "I": 1,
         "V": 5,
@@ -10,15 +11,22 @@ function romanToInteger(s)
         "C": 100,
         "D": 500,
         "M": 1000
-      };
+    };
 
-      let total = 0;
+    // initialize the result
+    let total = 0;
 
-      for (let i = 0; i < s.length; i++) {
-            if(i + 1 < s.length && romanLiterals[s[i]] < romanLiterals[s[i+1]]) total -= romanLiterals[s[i]]
-            else total += romanLiterals[s[i]];
-      }
-      return total;
+    //loop over every character in the string s
+
+    for (let i = 0; i < s.length; i++) {
+        // if the next index is not out of bounds and the character in the roman literal is less than the next character in the roman literal, substract is from total
+        if (i + 1 < s.length && romanLiterals[s[i]] < romanLiterals[s[i + 1]]) total -= romanLiterals[s[i]]
+        // if the current character is greater than or equal to the next character add to the total
+        else total += romanLiterals[s[i]];
+    }
+
+    // return the total
+    return total;
 }
 
 console.log(romanToInteger("MCMXCIV"));
