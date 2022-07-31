147d7f647babea0f2815bdd36b743d8fb2195022
eloquentcoder <eloquentintech@gmail.com>
2022-07-31 15:48:40 +0100

    Non Overlapping Intervals Javascript

---
+++ Intervals/NonOverlappingIntervals/erase_overlapping_intervals.js
@@ -0,0 +1,41 @@
+
+// the overall idea behind this problem is to go over the intervals and compare the 
+// starting and ending points of each interval after it has been sorted and check for 
+// the following, if the current interval's start is greater than or equal to the previous interval's end, it means
+// they are not overlapping. If the current interval's start is less than the previous interval's end, it means they 
+// are both overlapping so we increase the result. Then we set the previous non overlapping interval's end to the smallest end 
+// of both overlapping intervals. This way we are sure to not have to remove more numbers since the question is asking us to 
+// remove the least number of non overlapping intervals
+function eraseOverlapIntervals(intervals) {
+    // we need to sort the interval array so that it would be
+    // easier to compare the start and end values of intervals
+    intervals.sort();
+
+     // we initialize the result to 0
+    let result = 0;
+
+     // we set the previous end of the current end we are strutinizing to the first interval in the array
+    let prevEnd = intervals[0][1];
+
+        // starting from the second interval, we have to loop through the whole array
+    for (let i = 0; i < intervals.length; i++) {
+         // we use array destructuring to get the start end value of an interval
+        const [start, end] = intervals[i];
+
+        // if the current start is greater than the prevEnd, \
+        //which means they are not overlapping so we set the prevEnd to the current interval's end
+        if (start >= prevEnd) {
+            prevEnd = end;
+        } 
+        // else the start is less than the prevEnd so it means they are overlapping
+        // increase the value of result by 1. it means we have removed an overlapping interval
+        // now set the previous non overlapping interval's end to the smallest value between the
+        // currently scrutinized end the current interval's end
+        else {
+            result += 1;
+            prevEnd = Math.min(prevEnd, end);
+        }
+    }
+      // return the result;
+    return result;
+}
